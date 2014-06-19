e1cd065e724369af8d1e2d137fb7cddd90f134f2
Pumbaa <deploy-disney@expandtheroom.com>
2014-06-19 06:38:24 +0000

    add an option to not filter the blog description inside of wp_title(), by not loading the filter until wp_head, which should only be fired in templates right before the end of the <head> tag

--- trunk/tagline-rotator.php
+++ trunk/tagline-rotator.php
@@ -4,12 +4,12 @@
 Plugin Name: Tagline Rotator
 Plugin URI: http://neverblog.net/tagline-rotator-plugin-for-wordpress
 Description: Displays a random tagline from a database list. You can manage taglines through Settings->Tagline Rotator.
-Version: 2.3 
+Version: 2.3
 Author: Vasken Hauri
 Author URI: http://neverblog.net
 */
 
-/*  
+/*
 Copyright 2008-2011  Vasken Hauri  (email : vhauri (at) gmail dot com)
 
     This program is free software; you can redistribute it and/or modify
@@ -28,126 +28,194 @@ Copyright 2008-2011  Vasken Hauri  (email : vhauri (at) gmail dot com)
 
 */
 
-if( !class_exists('WP_Tagline_Rotator') ){
-	class WP_Tagline_Rotator{
+if
+( !class_exists('WP_Tagline_Rotator') )
+{
+	class WP_Tagline_Rotator
+	{
 
-		public function __construct(){
-			if(is_admin())
+		public function __construct()
+		{
+			if
+			(is_admin())
 				add_action('admin_menu', array($this, 'add_pages'));
+			elseif
+			(get_option( 'tagline_rotator_filter_wp_title' ))
+			{
+				add_action( 'wp_head', array( $this, 'add_filter' ) );
+			}
 			else
-				add_filter('bloginfo', array($this, 'filter_bloginfo'),11,2);
-			
-			register_activation_hook(__FILE__,array('WP_Tagline_Rotator', 'tagline_upgrade_check'));
+			{
+				add_filter('bloginfo', array($this, 'filter_bloginfo'), 11, 2);		
+			}
+				
+			register_activation_hook(__FILE__, array('WP_Tagline_Rotator', 'tagline_upgrade_check'));
 		}
-	
-		public static function add_pages() {
-				// Add a new submenu under Options:
-				add_options_page('Tagline Rotator', 'Tagline Rotator', 'manage_options', 'taglineoptions', array('WP_Tagline_Rotator', 'tagline_options_page'));
+		
+		public function add_filter() {
+			add_filter('bloginfo', array($this, 'filter_bloginfo'), 11, 2);
 		}
 
-		public static function tagline_upgrade_check(){
+		public static function add_pages()
+		{
+			// Add a new submenu under Options:
+			add_options_page('Tagline Rotator', 'Tagline Rotator', 'manage_options', 'taglineoptions', array('WP_Tagline_Rotator', 'tagline_options_page'));
+		}
+
+		public static function tagline_upgrade_check()
+		{
 			global $wpdb;
 			$old_taglines = $wpdb->get_results( "SELECT * FROM " . $wpdb->prefix . "tagline_rotator" );
 			$taglines = get_option('tagline_rotator_taglines');
-			
-			foreach($old_taglines as $old_tagline){
-				$taglines[] = $old_tagline->random_tagline;		
+
+			foreach
+			($old_taglines as $old_tagline)
+			{
+				$taglines[] = $old_tagline->random_tagline;
 			}
 
 			update_option('tagline_rotator_taglines', $taglines);
 		}
 
-		public static function tagline_options_page() {
+		public static function tagline_options_page()
+		{
 			$taglines = get_option('tagline_rotator_taglines');
+			$filter_wp_title = get_option('tagline_rotator_filter_wp_title');
 
-			if( !empty($_POST) && check_admin_referer('tagline-rotator-update-options', 'tagline-rotator-nonce') )
+			if
+			( !empty($_POST) && check_admin_referer('tagline-rotator-update-options', 'tagline-rotator-nonce') )
 			{
-			
-				if(isset($_POST['text'])){
+
+				if
+				(isset($_POST['text']))
+				{
 					$text = $_POST['text'];
-					foreach ($text as $key => $tagline){
-						if($tagline !== '')
+					foreach ($text as $key => $tagline)
+					{
+						if
+						($tagline !== '')
 							$taglines[$key] = wp_kses_post($tagline);
-						}
 					}
-				
+				}
+
 				//loop through the checkboxes to see what to delete
-				if(isset($_POST['box'])){
+				if
+				(isset($_POST['box']))
+				{
 					$box = $_POST['box'];
-					foreach ($box as $x){
+					foreach ($box as $x)
+					{
 						unset($taglines[$x]);
-					}	
-				}  
-				
+					}
+				}
+
 				// check for new tagline and insert if found
-				if(isset($_POST['new_tagline']) && $_POST['new_tagline'] !== ''){
+				if
+				(isset($_POST['new_tagline']) && $_POST['new_tagline'] !== '')
+				{
 					$new_tagline = wp_kses_post($_POST['new_tagline']);
 					$taglines[] = $new_tagline;
 				}
-				
+
+				//check whether we want to turn off the random tagline for wp_title
+				if
+				( isset( $_POST['filter_wp_title'] ) )
+				{
+					update_option( 'tagline_rotator_filter_wp_title', 1 );
+					$filter_wp_title = 1;
+				} else
+				{
+					delete_option( 'tagline_rotator_filter_wp_title' );
+					$filter_wp_title = 0;
+				}
+
 				update_option('tagline_rotator_taglines', $taglines);
 			}
-			?>
+?>
 
 			<!-- begin options page form -->
 			<div class="wrap">
-			<h2>Tagline Rotator Options</h2>
-			<p><strong>PLEASE NOTE:</strong> You must click 'Save Changes' in order to permanently add or delete a tagline.</p>
-			<form method="post">
-			<hr>
-			<h3>Add New Taglines</h3>
-			<table class="form-table">
-			<tr valign="top">
-			<th scope="row">Add a new tagline</th>
-			<td><input type="text" name="new_tagline" size="80" /></td></tr>
-			</table>
+				<h2>Tagline Rotator Options</h2>
+				<p><strong>PLEASE NOTE:</strong> You must click 'Save Changes' in order to permanently add or delete a tagline.</p>
+				<form method="post">
+					<hr>
+					<h3>Description Filters</h3>
+					<table class="form-table">
+						<tr valign="top">
+							<th scope="row">Don't randomize tagline in header</th>
+							<td><input type="checkbox" name="filter_wp_title" value="1" <?php checked( $filter_wp_title, 1 ); ?>></td>
+						</tr>
+					</table>
+					<h3>Add New Taglines</h3>
+					<table class="form-table">
+						<tr valign="top">
+							<th scope="row">Add a new tagline</th>
+							<td><input type="text" name="new_tagline" size="80" /></td></tr>
+					</table>
 
-			<p class="submit">
-			<input type="submit" name="Submit" value="<?php _e('Save Changes') ?>" />
-			</p>
-			<hr>
-			<h3>Delete Taglines</h3>
-			<table class="form-table">
+					<p class="submit">
+						<input type="submit" name="Submit" value="<?php _e('Save Changes') ?>" />
+					</p>
+					<hr>
+					<h3>Delete Taglines</h3>
+					<table class="form-table">
 			<?php wp_nonce_field('tagline-rotator-update-options', 'tagline-rotator-nonce'); ?>
 
 			<?php
-			foreach($taglines as $key => $tagline){
-			?>
+			if
+			( !empty( $taglines ) )
+			{
+				foreach
+				($taglines as $key => $tagline)
+				{
+?>
 
-			<tr valign="top">
-			<th scope="row">Delete this tagline</th>
-			<?php $sanitized_tagline = preg_replace('/"/','&quot;',$tagline); ?>
-			<td><input type="checkbox" name="box[<?php echo $key; ?>]" value="<?php echo $key;?>" /><input type="text" size="80" name="text[<?php echo $key; ?>]" value="<?php echo stripslashes($sanitized_tagline);?>" /><input type="hidden" name="vhtr_ids[]" value="<?php echo $key;?>" /></td></tr>
+						<tr valign="top">
+							<th scope="row">Delete this tagline</th>
+							<?php $sanitized_tagline = preg_replace('/"/', '&quot;', $tagline); ?>
+							<td><input type="checkbox" name="box[<?php echo $key; ?>]" value="<?php echo $key;?>" /><input type="text" size="80" name="text[<?php echo $key; ?>]" value="<?php echo stripslashes($sanitized_tagline);?>" /><input type="hidden" name="vhtr_ids[]" value="<?php echo $key;?>" />
+							</td>
+						</tr>
 
 			<?php
+				}
 			}
-			?>
-			</table>
-			<br>
-			<p class="submit">
-			<input type="submit" name="Submit" value="<?php _e('Save Changes') ?>" />
-			</p>
+?>
+					</table>
+					<br>
+					<p class="submit">
+						<input type="submit" name="Submit" value="<?php _e('Save Changes') ?>" />
+					</p>
 			<?php
 		}
 
-		public static function filter_bloginfo($output = '', $show){
-			if($show == 'description'){
+		public static function filter_bloginfo($output = '', $show)
+		{
+			if
+			($show == 'description')
+			{
 				$taglines = get_option('tagline_rotator_taglines');
-				foreach($taglines as $tagline){
+				foreach
+				($taglines as $tagline)
+				{
 					$tagline_array[] = $tagline;
 				}
-				if($tagline_array !== FALSE){
+				if
+				($tagline_array !== FALSE)
+				{
 					$tagline_counter = count($tagline_array);
-					$key = rand(0, $tagline_counter - 1);	
+					$key = rand(0, $tagline_counter - 1);
 					$output = $tagline_array[$key];
 				}
 			}
 			return stripslashes($output);
-		}	
+		}
 
 	}
 }//end WP_Tagline_Rotator class
 
-if( class_exists('WP_Tagline_Rotator')){
+if
+( class_exists('WP_Tagline_Rotator'))
+{
 	new WP_Tagline_Rotator;
 }
